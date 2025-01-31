f4d4e88b057a55593131a8d742ccefb73cfcd0b2
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-01-31 22:18:38 +0100

    add result

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const playerDisplay = document.querySelector('.game__player');
 const computerDisplay = document.querySelector('.game__computer');
-const results = document.querySelector('.game__result');
+const resultDisplay = document.querySelector('.game__result');
 
 const choices = ["rock", "paper", "scissors"];
+let result = "";
 
 const playGame = (playerChoices) => {
 
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-    
+
 };
